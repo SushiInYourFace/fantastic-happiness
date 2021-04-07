5291b9a5368f7fe2e9bddc8158790ba85c9705ba
SushiInYourFace <joel_olmsted@icloud.com>
2021-04-07 17:19:16 -0400

    changes

--- update.sh
+++ update.sh
@@ -3,9 +3,14 @@
 git fetch origin
 LOCALUPDATES=$(git log origin/main..HEAD --oneline)
 echo $LOCALUPDATES
-
 if ! [ -z "$LOCALUPDATES"]
 then
     exit 1
 fi
-e
+INCOMINGUPDATES=$(git log HEAD..origin/main --oneline)
+if [ -z "$INCOMINGUPDATES"]
+then
+    exit 1
+fi
+
+git pull
